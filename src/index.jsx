d097cd94ed9652881799850d81579f7b81a40f2c
oimo23 <oimo2338@gmail.com>
2018-05-02 14:19:56 +0900

    Redux化完了

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-// import App from './components/App';
-import SearchPage from './components/SearchPage';
+import App from './components/App';
 import reducer from './reducers/';
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <SearchPage
-      history={history}
-      location={location}
-    />
+    <App />
   </Provider>,
   document.querySelector('.container'),
 );
